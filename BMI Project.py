90b9330dd33f62cbdcaaea678a815b9532654095
SaraDobani <142623675+SaraDobani@users.noreply.github.com>
2023-08-18 16:13:43 -0400

    Add files via upload

---
+++ BMI Project.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+name = input("Enter you name: ")
+weight = int(input("Enter your weight in pound: "))
+height = int(input("Enter your height in inches: "))
+BMI = (weight * 703) / (height * height)
+print(BMI)
+if BMI>0:
+    if(BMI<18.5):
+        print(name +", you are underwight.")
+    elif (BMI<=24.9):
+        print(name +", you are normal weight.")
+    elif (BMI<29.9):
+        print(name +", you are overweight. You need to start exercising.")
+    elif (BMI<34.9):
+        print(name +", you are obese. You need to control your diet and exercise. ")
+    elif (BMI<39.9):
+        print(name +", you are severely obese.")
+    else:
+        print(name +", you are morbidly obese.")
+else:
+    print("Enter valid input")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+print(weight)
+
+
+# In[13]:
+
+
+Under 18.5	Underweight	Minimal
+18.5 - 24.9	Normal Weight	Minimal
+25 - 29.9	Overweight	Increased
+30 - 34.9	Obese	High
+35 - 39.9	Severely Obese	Very High
+40 and over	Morbidly Obese	Extremely High
+
+
+# In[14]:
+
+
+if BMI>0:
+    if(BMI<18.5):
+        print(name +", you are underwight.")
+    elif (BMI<=24.9):
+        print(name +", you are normal weight.")
+    elif (BMI<29.9):
+        print(name +", you are overweight.")
+    elif (BMI<34.9):
+        print(name +", you are obese.")
+    elif (BMI<39.9):
+        print(name +", you are severely obese.")
+    else:
+        print(name +", you are morbidly obese.")
+else:
+    print("Enter valid input")
+
+
+# In[ ]:
+
+
+
+
